d45025bcb6e190de0ae3134af60f0b586a16a7db
pcampbe8 <pcampbe8@hopper2.orc.gmu.edu>
2021-03-31 11:45:03 -0400

    Fixed bug in HEMCO config file, updated scripts, and updated C384 and C96 emissions.

--- HEMCO_Config/02.run_rect_to_cubed.sh
+++ HEMCO_Config/02.run_rect_to_cubed.sh
@@ -5,6 +5,10 @@
 export interp_method=conserve
 echo $interp_method
 
+declare -a fv3_ress=(C48 C96 C192 C384 C768)
+for fv3_res in "${fv3_ress[@]}"; do
+echo $fv3_res
+
 declare -a tiles=(tile1 tile2 tile3 tile4 tile5 tile6)
 for tile_num in "${tiles[@]}"; do
 echo $tile_num
@@ -14,14 +18,15 @@ for date in "${dates[@]}"; do
 echo $date
 
 export  srcfile=./output/HEMCO_sa.diagnostics.${date}.nc
-export  dstfile=/data/aqf/barryb/fv3grid/fv3grid/C384_grid_spec.${tile_num}.nc
-export  wgtfile=/data/aqf/patrickc/fv3_chem_emissions/weights_file_cubed_${tile_num}_FV3_${interp_method}.nc
+export  dstfile=/scratch/pcampbe8/UFS/fv3grid/${fv3_res}_grid_spec.${tile_num}.nc
+export  wgtfile=/scratch/pcampbe8/UFS/fv3grid/fv3_weights_file_${fv3_res}_${tile_num}_${interp_method}.nc
 export  infile=./output/HEMCO_sa.diagnostics.${date}.nc
-export  outfile=./output/regrid/HEMCO_sa.diagnostics.${date}_${tile_num}.nc
+export  outfile=./output/regrid/HEMCO_sa.diagnostics.${date}_${fv3_res}_${tile_num}.nc
 
 rm -f ${outfile}
 
 ncl rect_to_cubed.ncl
 
+done
 done
 done
--- HEMCO_Config/03.convert_units.sh
+++ HEMCO_Config/03.convert_units.sh
@@ -6,19 +6,24 @@
 
 rm output/regrid/*convert*.nc
 
+declare -a fv3_ress=(C48 C96 C192 C384 C768)
+
 declare -a times=(20190101 20190201 20190304 20190404 20190505 20190605 20190706 20190806 20190906 20191007 20191107 20191208)
 
 declare -a tiles=(tile1 tile2 tile3 tile4 tile5 tile6)
 
+for fv3_res in "${fv3_ress[@]}"; do
+echo $fv3_res
 for time_num in "${times[@]}"; do
 echo $time_num
 for tile_num in "${tiles[@]}"; do
 echo $tile_num
 
-ncap2 -s    SO2=SO2*5.62e10 output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${tile_num}.nc  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${tile_num}_convert.nc
-ncap2 -A -s OC=OC*1e9  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${tile_num}.nc  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${tile_num}_convert.nc
-ncap2 -A -s BC=BC*1e9  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${tile_num}.nc  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${tile_num}_convert.nc
-ncap2 -A -s PM25=PM25*1e9  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${tile_num}.nc  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${tile_num}_convert.nc
+ncap2 -s    SO2=SO2*5.62e10 output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${fv3_res}_${tile_num}.nc  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${fv3_res}_${tile_num}_convert.nc
+ncap2 -A -s OC=OC*1e9  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${fv3_res}_${tile_num}.nc  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${fv3_res}_${tile_num}_convert.nc
+ncap2 -A -s BC=BC*1e9  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${fv3_res}_${tile_num}.nc  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${fv3_res}_${tile_num}_convert.nc
+ncap2 -A -s PM25=PM25*1e9  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${fv3_res}_${tile_num}.nc  output/regrid/HEMCO_sa.diagnostics.${time_num}0000_${fv3_res}_${tile_num}_convert.nc
 
+done
 done
 done
